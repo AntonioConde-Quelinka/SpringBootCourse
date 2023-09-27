82dc488491015c23d4cd1455ae2f51db0ab0dddf
Toño Conde <taguito@gmail.com>
2023-09-27 18:10:04 +0200

    CORS Example (se deja comentado desactivado)

--- msalumnos_t/src/main/java/edu/cta/academy/controller/AlumnoController.java
+++ msalumnos_t/src/main/java/edu/cta/academy/controller/AlumnoController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +39,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 /**
  * Recibe HTTP Request y devuelve HTTP Responses
  */
+//@CrossOrigin(
+//		originPatterns = {"*"},
+//		methods = {RequestMethod.GET})
 @RestController
 @RequestMapping("/alumno")
 public class AlumnoController  {
