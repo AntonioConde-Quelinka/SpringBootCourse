ac073383797c74ed69def4189a2b2917eaf5b139
Toño Conde <taguito@gmail.com>
2023-09-27 17:03:25 +0200

    .

---
+++ msalumnos_t/src/main/java/edu/dta/academy/model/FraseChiquito.java
@@ -0,0 +1,31 @@
+package edu.dta.academy.model;
+
+public class FraseChiquito {
+	
+	private int id;
+	private String quote;
+	
+	public FraseChiquito() {
+	}
+	
+	public FraseChiquito(int id, String quote) {
+		super();
+		this.id = id;
+		this.quote = quote;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	
+	
+
+}
